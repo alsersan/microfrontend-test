41a7b0ba2fe9e22266e0b93fe4d16fe77484a23d
Álvaro Serrano <alsersan20@gmail.com>
2023-05-24 09:37:13 +0200

    Add parent-child navigation communication

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,24 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 const MarketingApp = () => {
   const ref = useRef();
+  const history = useHistory();
 
   useEffect(() => {
     if (ref.current) {
-      mount(ref.current);
+      const { onParentNavigate } = mount(ref.current, {
+        onNavigate({ pathname: nextPathname }) {
+          const { pathname } = history.location;
+
+          if (pathname !== nextPathname) {
+            history.push(nextPathname);
+          }
+        },
+      });
+
+      history.listen(onParentNavigate);
     }
   }, []);
 
