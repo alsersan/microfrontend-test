4a6fefdd9138feb1fcc5b0d4fff69378ead3f37c
Álvaro Serrano <alsersan20@gmail.com>
2023-05-26 09:16:36 +0200

    Fix public path in dev webpack config

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -3,10 +3,15 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
 
+const port = 8082;
+
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: `http://localhost:${port}/`,
+  },
   devServer: {
-    port: 8082,
+    port: port,
     historyApiFallback: true,
   },
   plugins: [
